3804d5593894403bd5b9293d19b90d4a2aa336e6
Akeksandr <sanek.pershin.68rus@gmail.com>
2022-07-07 20:36:17 +0300

    scripts-for-inventory-added

---
+++ Assets/Scripts/Enviroment/CollectibleItem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleItem : MonoBehaviour
+{
+    [SerializeField]
+    private string itemName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Managers.Inventory.AddItem(itemName);        
+        Destroy(this.gameObject);
+    }
+}
